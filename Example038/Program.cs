13656d267b432ea61ed4ab67da8dfa7b78c05e41
Vazgen <egiazarov.vazgen@icloud.com>
2022-11-12 16:39:53 +0300

    Example038

---
+++ Example038/Program.cs
@@ -0,0 +1,50 @@
+﻿// Программа, которая задаёт массив вещественных чисел 
+// и находит разницу между максимальным и минимальным элементом массива
+
+double[] array = new double[10];
+
+void FillArray(double[] collection)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 200) + random.NextDouble();
+    }
+}
+
+void PrintArray(double[] col)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+double DiffBetweenMaxAndMinElements()
+{
+    double max = array[0];
+    double min = array[0];
+    double diff = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        else if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    diff = max - min;
+    Console.WriteLine("Максимальный элемент массива: " + max);
+    Console.WriteLine("Минимальный элемент массива: " + min);
+    Console.WriteLine("Разница между максимальным и минимальным элементом массива: " + diff);
+    return diff;
+}
+
+FillArray(array);
+Console.WriteLine("Массив: ");
+PrintArray(array);
+Console.WriteLine();
+DiffBetweenMaxAndMinElements();
